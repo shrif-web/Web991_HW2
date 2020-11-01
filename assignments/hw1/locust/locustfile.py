568625db93ad754df1ce3036d278055b780369ba
parhamsaremi <pa.sa.13792000@outlook.com>
2020-11-01 19:48:15 +0330

    added locust file

---
+++ assignments/hw1/locust/locustfile.py
@@ -0,0 +1,28 @@
+import time, random
+from locust import HttpUser, task, between, tag
+
+class QuickstartUser(HttpUser):
+
+    @tag('node')
+    @tag('sha')
+    @task
+    def sha256(self):
+        self.client.post("/nodejs/sha256", {"num1": "12","num2":"13"})
+    
+    @tag('node')
+    @tag('write')
+    @task
+    def write_node(self):
+        self.client.get("/nodejs/write?lineNumber="+"10")
+
+    @tag('go')
+    @tag('sha')
+    @task
+    def sha256_go(self):
+        self.client.post("/go/sha256", {"num1": "12","num2":"13"})
+
+    @tag('go')
+    @tag('write')
+    @task
+    def write_go(self):
+        self.client.get("/go/write?lineNumber=12")
