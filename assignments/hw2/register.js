4c632ce960980f34b7e56c98d771c2c78d445fd2
parhamsaremi <pa.sa.13792000@outlook.com>
2020-11-19 15:50:31 +0330

    alert box for errors

--- assignments/hw2/register.js
+++ assignments/hw2/register.js
@@ -11,12 +11,12 @@ fillFormRegister = function(){
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="تکرار رمز عبور">
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2">
                 <label class="form-check-label" for="invalidCheck2">
                   قوانین و شرایط را می‌پذیرم
                 </label>
             </div>              
-            <button type="submit" class="btn btn-primary btn-sm btn-block">ثبت نام</button>
+            <button type="submit" class="btn btn-primary btn-sm btn-block" onclick="checkForm()">ثبت نام</button>
         </div>
     </form>`
     var element = document.getElementById("registerTab")
@@ -44,4 +44,22 @@ fillFormLogin = function(){
     element = document.getElementById("registerTab")
     element.classList.remove("active")
     element.style.backgroundColor = "#acb3fa"
+}
+
+showAlert = function(){
+    var element = document.getElementById("alertContainer")
+    element.innerHTML = `<div id = "alertBox1 align-item-center" class="alert alert-info alert-dismissible fade show fade-in" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="closeAlert()">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    <strong>خطا</strong>
+    </div>`
+}
+closeAlert = function(){
+    var element = document.getElementById("alertBox1")
+    element.alert('close')
+}
+checkForm = function(){
+    //todo
+    showAlert() 
 }
