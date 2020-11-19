91122324deefcfbdd3345d030fdf5b40fc2bc4a9
parhamsaremi <pa.sa.13792000@outlook.com>
2020-11-19 19:26:06 +0330

    editted alert boxes

--- assignments/hw2/register.js
+++ assignments/hw2/register.js
@@ -1,14 +1,16 @@
 fillFormRegister = function () {
+    var alert = document.getElementById("alertContainer")
+    alert.innerHTML = ``
     document.getElementById("logRegForm").innerHTML = `<form>
         <div class = "form-group align-items-center mx-sm-3">
             <div class="form-group" style = "margin-top: 10px;">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ایمیل">
+                <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="ایمیل" oninput="changeDir(this)">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="رمز عبور">
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="رمز عبور" oninput="changeDir(this)">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="تکرار رمز عبور">
+                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="تکرار رمز عبور" oninput="changeDir(this)">
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="" id="invalidCheck2">
@@ -19,21 +21,24 @@ fillFormRegister = function () {
             <button type="submit" class="btn btn-primary btn-sm btn-block" onclick="checkFormRegister()">ثبت نام</button>
         </div>
     </form>`
-    var element = document.getElementById("registerTab")
+    element = document.getElementById("registerTab")
     element.classList.add("active")
     element.style.backgroundColor = "white"
     element = document.getElementById("loginTab")
     element.classList.remove("active")
     element.style.backgroundColor = "#acb3fa"
+    
 }
 fillFormLogin = function () {
+    var alert = document.getElementById("alertContainer")
+    alert.innerHTML = ``
     document.getElementById("logRegForm").innerHTML = `<form>
         <div class = "form-group align-items-center mx-sm-3">
             <div class="form-group" style="margin-top:55px;">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ایمیل">
+                <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="ایمیل" oninput="changeDir(this)">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="رمز عبور">
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="رمز عبور" oninput="changeDir(this)">
             </div>
             <button type="submit" class="btn btn-primary btn-sm btn-block" onclick="checkFormLogin()">ورود</button>
         </div>
@@ -46,13 +51,14 @@ fillFormLogin = function () {
     element.style.backgroundColor = "#acb3fa"
 }
 
-showAlert = function (str) {
+showAlert = function (str, type) {
     var element = document.getElementById("alertContainer")
-    element.innerHTML = `<div id = "alertBox1 align-item-center" class="alert alert-info alert-dismissible fade show fade-in" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="closeAlert()">
-      <span aria-hidden="true">&times;</span>
-    </button>
-    <strong>${str}</strong>
+    element.innerHTML = `
+    <div id = "alertBox1" class="alert alert-${type} alert-dismissible fade show fade-in  align-item-center form-group mx-sm-3" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="closeAlert()">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <strong>${str}</strong>
     </div>`
 }
 
@@ -62,33 +68,69 @@ closeAlert = function () {
 }
 
 checkFormRegister = function () {
-    var email = document.getElementById('exampleInputEmail1').value;
+    var alert = document.getElementById("alertContainer")
+    alert.innerHTML = ``
+    var email = document.getElementById('email').value;
     var password = document.getElementById('exampleInputPassword1').value;
     var repeatPassword = document.getElementById('exampleInputPassword2').value;
     var agreeTerms = document.getElementById('invalidCheck2').checked;
     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!agreeTerms) {
-        showAlert('لطفا قوانین و شرایط را قبول کنید');
+    if(!email){
+        showAlert('ایمیل خالی می‌باشد.','danger')
     }
     else if (!emailRegex.test(email)) {
-        showAlert('فرمت ایمیل ورودی صحیح نمی‌باشد');
+        showAlert('فرمت ایمیل ورودی صحیح نمی‌باشد','danger');
+    }
+    else if (!password){
+        showAlert('رمز عبور خالی می‌باشد.','danger')
+    }
+    else if (!repeatPassword){
+        showAlert('تکرار رمز عبور خالی می‌باشد.','danger')
     }
     else if (password != repeatPassword) {
-        showAlert('رمز عبور و تکرار آن یکسان نمی‌باشد');
+        showAlert('رمز عبور و تکرار آن یکسان نمی‌باشد','danger');
+    }
+    else if (!agreeTerms) {
+        showAlert('لطفا قوانین و شرایط را قبول کنید','danger');
+    }
+    else{
+        showAlert('ثبت‌نام موفقیت آمیز بود','success')
     }
 }
 
 checkFormLogin = function () {
-    var email = document.getElementById('exampleInputEmail1').value;
+    var alert = document.getElementById("alertContainer")
+    alert.innerHTML = ``
+    var email = document.getElementById('email').value;
     var password = document.getElementById('exampleInputPassword1').value;
     var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!emailRegex.test(email)) {
-        showAlert('فرمت ایمیل ورودی صحیح نمی‌باشد');
+    if(!email){
+        showAlert('ایمیل خالی می‌باشد.','danger')
+    }
+    else if (!emailRegex.test(email)) {
+        showAlert('فرمت ایمیل ورودی صحیح نمی‌باشد','danger');
+    }
+    else if (!password){
+        showAlert('رمز عبور خالی می‌باشد.','danger')
     }
     else if (email != '1111@gmail.com') {
-        showAlert('ایمیل ورودی صحیح نمی‌باشد');
+        showAlert('ایمیل ورودی صحیح نمی‌باشد','danger');
     }
     else if (password != '1111') {
-        showAlert('رمز عبور صحیح نمی‌باشد‌');
+        showAlert('رمز عبور صحیح نمی‌باشد‌','danger');
+    }
+    else{
+        showAlert('ورود موفقیت آمیز بود','success')
+    }
+
+}
+
+
+changeDir = function(elem){
+    if(!elem.value){
+        elem.dir = "rtl"
+    }
+    else {
+        elem.dir = "ltr"
     }
 }
