b37fe204c367246a9cb09f5c6688fa00e57f7a06
sepehrs1378 <sepehr.safari8731@gmail.com>
2020-11-19 17:16:10 +0330

    Finished alert container. Now it will show proper message for invalid inputs.

--- assignments/hw1/nodejs/index.js
+++ assignments/hw1/nodejs/index.js
@@ -14,7 +14,6 @@ app.post('/sha256', (req, res) => {
   var num2 = req.body.num2;
   num1 = parseInt(num1);
   num2 = parseInt(num2);
-  console.log(`${num1} ${num2}`)
   if(!Number.isInteger(num1) || !Number.isInteger(num2)){
     console.log('invalid input...')
     res.statusCode = 400;
@@ -25,9 +24,7 @@ app.post('/sha256', (req, res) => {
     return;
   }
   var sum = num1 + num2;
-  console.log(`sum: ${sum}`);
   var encryptedSum = hash.update(sum.toString()).digest('hex');
-  console.log(`sum to encrypted by sha256: ${encryptedSum}`);
   res.statusCode = 200;
   res.json({
     'result' : encryptedSum,
@@ -56,7 +53,6 @@ app.get('/write', (req, res) => {
     });
     return;
   }
-  console.log(`lineNumber: ${lineNumber}`);
   fs.readFile('./text.txt', 'ascii', function (err,data) {
       if (err) {
       console.log(err);
@@ -64,7 +60,6 @@ app.get('/write', (req, res) => {
     }
     var splittedData=data.split('\n');
     var readLine=splittedData[lineNumber-1];
-    console.log(`read line: ${readLine}`);
     res.statusCode = 200;
     res.json({
       'result' : readLine,
--- assignments/hw2/register.js
+++ assignments/hw2/register.js
@@ -1,4 +1,4 @@
-fillFormRegister = function(){
+fillFormRegister = function () {
     document.getElementById("logRegForm").innerHTML = `<form>
         <div class = "form-group align-items-center mx-sm-3">
             <div class="form-group" style = "margin-top: 10px;">
@@ -8,7 +8,7 @@ fillFormRegister = function(){
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="رمز عبور">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="تکرار رمز عبور">
+                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="تکرار رمز عبور">
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="" id="invalidCheck2">
@@ -16,7 +16,7 @@ fillFormRegister = function(){
                   قوانین و شرایط را می‌پذیرم
                 </label>
             </div>              
-            <button type="submit" class="btn btn-primary btn-sm btn-block" onclick="checkForm()">ثبت نام</button>
+            <button type="submit" class="btn btn-primary btn-sm btn-block" onclick="checkFormRegister()">ثبت نام</button>
         </div>
     </form>`
     var element = document.getElementById("registerTab")
@@ -26,7 +26,7 @@ fillFormRegister = function(){
     element.classList.remove("active")
     element.style.backgroundColor = "#acb3fa"
 }
-fillFormLogin = function(){
+fillFormLogin = function () {
     document.getElementById("logRegForm").innerHTML = `<form>
         <div class = "form-group align-items-center mx-sm-3">
             <div class="form-group" style="margin-top: 88px;">
@@ -35,7 +35,7 @@ fillFormLogin = function(){
             <div class="form-group">
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="رمز عبور">
             </div>
-            <button type="submit" class="btn btn-primary btn-sm btn-block">ورود</button>
+            <button type="submit" class="btn btn-primary btn-sm btn-block" onclick="checkFormLogin()">ورود</button>
         </div>
     </form>`
     var element = document.getElementById("loginTab")
@@ -46,20 +46,49 @@ fillFormLogin = function(){
     element.style.backgroundColor = "#acb3fa"
 }
 
-showAlert = function(){
+showAlert = function (str) {
     var element = document.getElementById("alertContainer")
     element.innerHTML = `<div id = "alertBox1 align-item-center" class="alert alert-info alert-dismissible fade show fade-in" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="closeAlert()">
       <span aria-hidden="true">&times;</span>
     </button>
-    <strong>خطا</strong>
+    <strong>${str}</strong>
     </div>`
 }
-closeAlert = function(){
-    var element = document.getElementById("alertBox1")
+
+closeAlert = function () {
+    var element = document.getElementById('alertBox1')
     element.alert('close')
 }
-checkForm = function(){
-    //todo
-    showAlert() 
+
+checkFormRegister = function () {
+    var email = document.getElementById('exampleInputEmail1').value;
+    var password = document.getElementById('exampleInputPassword1').value;
+    var repeatPassword = document.getElementById('exampleInputPassword2').value;
+    var agreeTerms = document.getElementById('invalidCheck2').checked;
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!agreeTerms) {
+        showAlert('لطفا قوانین و شرایط را قبول کنید');
+    }
+    else if (!emailRegex.test(email)) {
+        showAlert('فرمت ایمیل ورودی صحیح نمی‌باشد');
+    }
+    else if (password != repeatPassword) {
+        showAlert('رمز عبور و تکرار آن یکسان نمی‌باشد');
+    }
+}
+
+checkFormLogin = function () {
+    var email = document.getElementById('exampleInputEmail1').value;
+    var password = document.getElementById('exampleInputPassword1').value;
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegex.test(email)) {
+        showAlert('فرمت ایمیل ورودی صحیح نمی‌باشد');
+    }
+    else if (email != '1111@gmail.com') {
+        showAlert('ایمیل ورودی صحیح نمی‌باشد');
+    }
+    else if (password != '1111') {
+        showAlert('رمز عبور صحیح نمی‌باشد‌');
+    }
 }
